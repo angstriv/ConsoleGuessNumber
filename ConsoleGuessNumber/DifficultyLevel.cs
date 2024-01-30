436813a1b5bf7a089709d70dd67927ec6b0815bb
Xu <Xu@DESKTOP-F2M7E20>
2024-01-30 22:56:49 +0800

    添加项目文件。

---
+++ ConsoleGuessNumber/DifficultyLevel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGuessNumber
+{
+    public static class DifficultyLevel
+    {
+
+        public static int GetEasyNumber()//简单难度的数字
+        {
+            int minValue = RandomNumber.GetIntRandom(1,500);
+            int maxValue = RandomNumber.GetIntRandom(500,5000);
+            int finalNumber = RandomNumber.GetIntRandom(minValue,maxValue);
+
+            return finalNumber;
+        }
+
+        public static double GetMediumNumber()//中等难度的数字
+        {
+            double minValue = RandomNumber.GetDoubleRandom(1.1, 500.1, 1);
+            double maxValue = RandomNumber.GetDoubleRandom(500.1, 5000.1, 1);
+            double finalNumber = RandomNumber.GetDoubleRandom(minValue,maxValue, 1);
+
+            return finalNumber;
+        }
+
+        public static double GetDifficultNumber()////困难难度的数字
+        {
+            double minValue = RandomNumber.GetDoubleRandom(1.11, 500.11, 2);
+            double maxValue = RandomNumber.GetDoubleRandom(500.11, 5000.11, 2);
+            double finalNumber = RandomNumber.GetDoubleRandom(minValue, maxValue, 2);
+
+            return finalNumber;
+        }
+    }
+}
---
+++ ConsoleGuessNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace ConsoleGuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("欢迎游玩猜数字游戏");
+            double answerNumber = 0;
+
+            //用户选择难度
+            while (true)
+            {
+                Console.Write("请选择难度:");
+                string dLevel = Console.ReadLine();//用户选择难度
+                if (dLevel == "easy")
+                {
+                    answerNumber = DifficultyLevel.GetEasyNumber();//获取简单数字
+                    break;
+                }
+                else if (dLevel == "medium")
+                {
+                    answerNumber = DifficultyLevel.GetMediumNumber();//获取中等数字
+                    break;
+                }
+                else if (dLevel == "difficult")
+                {
+                    answerNumber = DifficultyLevel.GetDifficultNumber();//获取困难数字
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("难度错误,请重新输入!");
+                    continue;
+                }
+            }
+            
+            //用户开始猜数字
+            while (true)
+            {
+                //获取用户输入的数字
+                Console.Write("请输入想猜的数字:");
+                string input = Console.ReadLine();
+                double inputNumber = Convert.ToDouble(input);
+
+                //比较2个数
+                double difference = inputNumber - answerNumber;
+                if (difference > 0)//inputNumber > answerNumbe
+                {
+                    Console.WriteLine("你输入的大了");
+                    continue;
+
+                }
+                else if (difference < 0)//inputNumber < answerNumbe
+                {
+                    Console.WriteLine("你输入的小了");
+                    continue;
+
+                }
+                else if(difference == 0)
+                {
+                    Console.WriteLine("恭喜你,猜对了!");
+                    break;
+                } 
+            }
+
+
+
+        }
+    }
+}
